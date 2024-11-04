e7aa41d615a006ed2fee1bae79e1efc8a418e534
Nishika Mouriya <mouriyanisika23@gmail.com>
2024-11-05 01:41:52 +0530

    Done

--- MinorAssignment_2/q15.py
+++ MinorAssignment_2/q15.py
@@ -1,15 +1,15 @@
 def perfect(num):
     if(num<1):
-        return 0
+        return 0 #not a natural number
     sum=0
     for i in range(1,num):
         if(num%i==0):
             sum+=i
     
     if(num==sum):
-        return  1
+        return  1  #for perfect number
     else:
-        return -1    
+        return -1    #for not perfect number
     
 num=int(input("Enter a number : "))
 ans=perfect(num)
@@ -21,7 +21,13 @@ else:
     print(f"Number is not a natural number {num}")
         
     
-        
+'''
+o/p
+Enter a number : 9
+Not a Perfect number : 9
+Enter a number : -3
+Number is not a natural number -3
+'''       
             
             
         
